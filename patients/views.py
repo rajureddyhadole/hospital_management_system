6bbbbbb0bf9c3ab5afe3ed995fb216798631d3f6
Rajureddy Hadole <rajureddyhadole452@gmail.com>
2026-04-07 19:14:39 +0530

    added edit, create and delete patients

--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Patient
 from django.contrib.auth.decorators import login_required
 
@@ -7,3 +7,39 @@ from django.contrib.auth.decorators import login_required
 def patient_list(request):
   patients = Patient.objects.all()
   return render(request, 'list.html', {"patients": patients})
+
+@login_required
+def patient_edit(request, pk):
+  patient = get_object_or_404(Patient, pk=pk)
+
+  if request.method == 'POST':
+    patient.name = request.POST.get('name')
+    patient.phone = request.POST.get('phone')
+    patient.email = request.POST.get('email')
+    patient.save()
+    return redirect('/patients')
+  return render(request, 'patient_edit.html', {'patient': patient})
+
+
+@login_required
+def patient_create(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+    email = request.POST.get('email')
+
+    Patient.objects.create(
+      name=name,
+      phone=phone,
+      email=email
+    )
+    return redirect('/patients')
+  return render(request, 'patient_create.html')
+
+
+@login_required
+def patient_delete(request, pk):
+  patient = get_object_or_404(Patient, pk=pk)
+
+  patient.delete()
+  return redirect('/patients')
