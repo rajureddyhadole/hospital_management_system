34f4396e32c7f308c5649f95fa69251f48ba95e6
Rajureddy Hadole <rajureddyhadole452@gmail.com>
2026-04-08 18:13:02 +0530

    added crud operations for doctors

--- doctors/models.py
+++ doctors/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Doctor(models.Model):
   name = models.CharField(max_length=200)
-  specification = models.CharField(max_length=200)
+  specialization = models.CharField(max_length=200)
 
   def __str__(self):
     return self.name
