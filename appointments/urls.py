1543386a2051708ac8f8ca4b23dfe8d04e59da0a
Rajureddy Hadole <rajureddyhadole452@gmail.com>
2026-04-10 20:26:20 +0530

    added create and delete operations for appointments

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import appointments_list, appointment_edit
+from .views import appointments_list, appointment_edit, appointment_delete, appointment_create
 
 urlpatterns = [
   path('', appointments_list),
-  path('<int:pk>/edit/', appointment_edit)
+  path('<int:pk>/edit/', appointment_edit),
+  path('<int:pk>/delete/', appointment_delete),
+  path('create/', appointment_create)
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Appointment
+from patients.models import Patient
+from doctors.models import Doctor
 
 # Create your views here.
 @login_required
@@ -10,6 +12,7 @@ def appointments_list(request):
   return render(request, 'appointments_list.html', {'appointments': appointments})
 
 
+@login_required
 def appointment_edit(request, pk):
   appointment = get_object_or_404(Appointment, pk=pk)
 
@@ -26,3 +29,39 @@ def appointment_edit(request, pk):
     return redirect('/appointments')
 
   return render(request, 'appointment_edit.html', {'appointment': appointment})
+
+
+@login_required
+def appointment_delete(request, pk):
+  appointment = get_object_or_404(Appointment, pk=pk)
+
+  appointment.delete()
+
+  return redirect('/appointments')
+
+
+@login_required
+def appointment_create(request):
+
+  if request.method == 'POST':
+    patient = Patient.objects.get(id=request.POST.get('patient'))
+    doctor = Doctor.objects.get(id=request.POST.get('doctor'))
+
+    from datetime import datetime
+    time = datetime.fromisoformat(request.POST.get('time'))
+
+    status = request.POST.get('status')
+
+    Appointment.objects.create(
+      patient = patient,
+      doctor = doctor,
+      appointment_time = time,
+      appointment_status = status
+    )
+    return redirect('/appointments')
+  
+  #GET request
+  patients = Patient.objects.all()
+  doctors = Doctor.objects.all()
+
+  return render(request, 'appointment_create.html', {'patients': patients, 'doctors': doctors})
